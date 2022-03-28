b1f4590630eb1d377eb766900ce2d049bf053df4
Luana Lessa <lessalsn@gmail.com>
2022-03-28 20:32:55 -0300

    chore: Code review

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return(
-              <div className="LogoContainer">
-                <img src='./static/logo-pequeno.png' alt='logo'/>
-                <h1 className="textLogo">ZapRecall</h1>
-            </div>
-    )
-}
